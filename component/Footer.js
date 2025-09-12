445b6c5d24e4ba315b8d981c957dcaf274d3c316
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-12 16:47:58 +0700

    fix style Footer

--- component/Footer.js
+++ component/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
     
   return (
 
-    <footer className="w-full py-4 text-sm text-center text-gray-600 bg-gray-100 border-t">
+    <footer className="w-full py-3 text-sm text-center text-gray-600 bg-gray-200 border-t">
       &copy; {new Date().getFullYear()} บริษัท โรงงานผลิตภัณฑ์อาหารไทย จำกัด
     </footer>
 
