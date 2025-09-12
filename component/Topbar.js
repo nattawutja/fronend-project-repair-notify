5b2fe74532e26224da7738cda1821e509768be7c
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-12 13:35:55 +0700

    add Topbar

--- component/Topbar.js
+++ component/Topbar.js
@@ -9,30 +9,28 @@ export default function TopBar() {
     if(pathname == '/'){
         return null;
     }
-    
-  return (
-
-    
-
-    <nav className="text-gray-600 bg-gray-100 shadow-md">
-      <div className="flex items-center justify-between px-4 py-3 mx-auto max-w-7xl">
-        {/* Logo */}
-        <div className="text-xl font-bold tracking-wide">
-         
-        </div>
-
-        {/* Menu Items */}
-        <div className="hidden space-x-6 md:flex">
         
+    return (
+
+        <nav className="text-gray-600 bg-gray-100 shadow-md">
+        <div className="flex items-center justify-between px-4 py-3 mx-auto max-w-7xl">
+            {/* Logo */}
+            <div className="text-xl font-bold tracking-wide">
+            
+            </div>
+
+            {/* Menu Items */}
+            <div className="hidden space-x-6 md:flex">
+            
+            </div>
+
+            {/* Logout button */}
+            <div className="inline-flex">
+            <Link href="/" className="transition hover:text-gray-200">&nbsp;ออกจากระบบ</Link>&nbsp; <FaSignOutAlt size={16} className="mt-1"/>
+            </div>
         </div>
-
-        {/* Logout button */}
-        <div className="inline-flex">
-        <Link href="/" className="transition hover:text-gray-200">&nbsp;ออกจากระบบ</Link>&nbsp; <FaSignOutAlt size={16} className="mt-1"/>
-        </div>
-      </div>
-    </nav>
+        </nav>
 
 
-  );
+    );
 }
