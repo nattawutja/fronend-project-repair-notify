9820fffd94083ec0778bb282ffbe550cef644410
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-12 16:48:48 +0700

    fix link to button logout

--- component/Topbar.js
+++ component/Topbar.js
@@ -6,29 +6,43 @@ export default function TopBar() {
     const pathname = usePathname();
     console.log(pathname);
 
+
+
+
     if(pathname == '/'){
         return null;
     }
         
+    const handleLogout = () => {
+        localStorage.clear();
+        sessionStorage.clear();
+        window.location.href = "/";
+    };
+
     return (
 
-        <nav className="text-gray-600 bg-gray-100 shadow-md">
-        <div className="flex items-center justify-between px-4 py-3 mx-auto max-w-7xl">
-            {/* Logo */}
-            <div className="text-xl font-bold tracking-wide">
-            
-            </div>
+        <nav className="text-gray-600 bg-gray-200 shadow-md">
+            <div className="flex items-center justify-between px-4 py-3 mx-auto max-w-7xl">
+                {/* Logo */}
+                <div className="text-xl font-bold tracking-wide">
+                
+                </div>
 
-            {/* Menu Items */}
-            <div className="hidden space-x-6 md:flex">
-            
-            </div>
+                {/* Menu Items */}
+                <div className="hidden space-x-6 md:flex">
+                
+                </div>
 
-            {/* Logout button */}
-            <div className="inline-flex">
-            <Link href="/" className="transition hover:text-gray-200">&nbsp;ออกจากระบบ</Link>&nbsp; <FaSignOutAlt size={16} className="mt-1"/>
+                {/* Logout button */}
+  
+
+                <div className="inline-flex">
+                    <button onClick={handleLogout} className="flex items-center gap-2 cursor-pointer hover:underline">
+                        ออกจากระบบ <FaSignOutAlt size={16} className="mt-1"/>
+                    </button>
+                </div>
+   
             </div>
-        </div>
         </nav>
 
 
