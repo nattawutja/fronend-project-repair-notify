cbf93e96ed0f49a00d72bc97132dfc03b8b9668f
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-13 14:08:02 +0700

    fix get localstoragename useeffect when path name changes

--- component/Topbar.js
+++ component/Topbar.js
@@ -1,13 +1,20 @@
 import Link from "next/link";
-import { FaSignOutAlt } from "react-icons/fa";
+import { FaSignOutAlt,FaUserCircle } from "react-icons/fa";
 import { usePathname } from 'next/navigation'
+import { useEffect, useState } from "react";
 
-export default function TopBar() {
+export default function TopBar({ isLoading }) {
+    const [fullName , setFullname] = useState("");
     const pathname = usePathname();
-    console.log(pathname);
 
+    useEffect(() => {
 
+        const storedName = localStorage.getItem("fullname");
+        if (storedName) {
+            setFullname(storedName);
+        }
 
+    }, [pathname]);
 
     if(pathname == '/'){
         return null;
@@ -18,7 +25,7 @@ export default function TopBar() {
         sessionStorage.clear();
         window.location.href = "/";
     };
-
+    
     return (
 
         <nav className="text-gray-600 bg-gray-200 shadow-md">
@@ -37,6 +44,7 @@ export default function TopBar() {
   
 
                 <div className="inline-flex">
+                    <span className="flex text-black me-2"> <FaUserCircle size={16} className="mt-1 me-2"/> {fullName}</span>
                     <button onClick={handleLogout} className="flex items-center gap-2 cursor-pointer hover:underline">
                         ออกจากระบบ <FaSignOutAlt size={16} className="mt-1"/>
                     </button>
