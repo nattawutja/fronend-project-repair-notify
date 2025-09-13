bb3ea35f53a1eb97d2d30ec6a56d15b2d3c2d27d
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-13 14:09:49 +0700

    call function sessiontimeout

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
+import React,{ useState,useEffect } from 'react';
 import TopBar from '../component/Topbar'
 import Footer  from '../component/Footer'
-
+import { setupSessionTimeout } from './utils/sessionTimeout'
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    // สร้าง session timeout
+    const { init, cleanup } = setupSessionTimeout(() => {
+      alert("Session หมดอายุ กรุณาเข้าสู่ระบบใหม่");
+      localStorage.clear();
+      window.location.href = "/";
+    });
+
+    init(); // เริ่มจับเวลา
+
+    return () => {
+      cleanup(); // ล้าง event listener ตอน unmount
+    };
+
+    
+  }, []);
+
+
   return (
     <>
       <TopBar/>
