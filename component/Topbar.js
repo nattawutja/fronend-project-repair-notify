aae78872d4265770a42285c15332718440858207
nattawut.ch <b.nattawut79@gmail.com>
2025-10-04 14:12:10 +0700

    modify topbar

--- component/Topbar.js
+++ component/Topbar.js
@@ -38,34 +38,38 @@ export default function TopBar({ isLoading }) {
     return (
 
         <nav className="text-gray-600 bg-gray-200 shadow-md">
-            <div className="flex items-center justify-between px-4 py-3 mx-auto max-w-7xl">
-                {/* Logo */}
-                <div className="text-xl font-bold tracking-wide bg-white px-2 py-2 rounded-lg border">
-                    <button onClick={handleHome} className="flex items-center gap-2 cursor-pointer hover:underline">
-                    <Image
-                    src="/waiwailogo.png"
-                    alt="My Photo"
-                    width={70}
-                    height={50}
-                    />
-                    </button>
-                </div>
+            <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between py-4">
+                    {/* Logo */}
+                    <div className="flex items-center space-x-4">
+                        <div className="text-xl font-bold tracking-wide bg-white px-2 py-2 rounded-lg border">
+                            <button onClick={handleHome} className="flex items-center gap-2 cursor-pointer hover:underline">
+                            <Image
+                            src="/waiwailogo.png"
+                            alt="My Photo"
+                            width={70}
+                            height={50}
+                            />
+                            </button>
+                        </div>
+                        <div className="ms-2">
+                            <h1 className="text-4xl font-bold gradient-text">Online Repair Notification System</h1>
+                            <p>ระบบแจ้งซ่อมออนไลน์</p>
+                        </div>
+                    </div>
+                   
 
-                {/* Menu Items */}
-                <div className="hidden space-x-6 md:flex">
-                
-                </div>
+                    {/* Logout button */}
+    
 
-                {/* Logout button */}
-  
-
-                <div className="inline-flex">
-                    <span className="flex text-black me-2"> <FaUserCircle size={16} className="mt-1 me-2"/> {fullName}  {divisionname}</span>
-                    <button onClick={handleLogout} className="flex items-center gap-2 cursor-pointer hover:underline">
-                        ออกจากระบบ <FaSignOutAlt size={16} className="mt-1"/>
-                    </button>
+                    <div className="inline-flex">
+                        <span className="flex text-black me-2"> <FaUserCircle size={16} className="mt-1 me-2"/> {fullName}  {divisionname}</span>
+                        <button onClick={handleLogout} className="flex items-center gap-2 cursor-pointer hover:underline">
+                            ออกจากระบบ <FaSignOutAlt size={16} className="mt-1"/>
+                        </button>
+                    </div>
+    
                 </div>
-   
             </div>
         </nav>
 
